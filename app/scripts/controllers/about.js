a0eac3124e667c8587f1d90e866803b78ad45a26
wilkhu90 <sumeet.wilkhu@gmail.com>
2016-04-24 16:26:11 -0500

    First Commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name sumeetProfileApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the sumeetProfileApp
+ */
+angular.module('sumeetProfileApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name sumeetProfileApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the sumeetProfileApp
+ */
+angular.module('sumeetProfileApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
